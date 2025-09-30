2cf9485ca80409b961a5e66ce1572e2616aedc3f
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-09-30 16:42:15 +0300

    add pixabay-api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,7 +5,29 @@ const API_KEY = '52515106-cf2fe0ed90e49660ada5f5535';
 const BASE_URL = 'https://pixabay.com/api/';
 
 function getImagesByQuery(query) {
-  axios(BASE_URL)
-    .then(res => {})
-    .catch(error => {});
+  return axios
+    .get(BASE_URL, getParams(query))
+    .then(res => {
+      const images = response.data.hits;
+
+      if (!images || !images.length) {
+        showError('Немає обраних зображень');
+      }
+    })
+    .catch(error => {
+      showError(error.message);
+      return [];
+    });
+}
+
+function getParams(query) {
+  return {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: horizontal,
+      safesearch: true,
+    },
+  };
 }
